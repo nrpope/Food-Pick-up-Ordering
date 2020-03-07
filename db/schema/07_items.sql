538cef321b762e6d23232420592b9670eece92e0
Bobby Uno <robert.y.uno@gmail.com>
2020-03-07 22:42:22 +0000

    updated items table, added order_items table

--- db/schema/07_items.sql
+++ db/schema/07_items.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  menu_category VARCHAR(255),
+  category_Id VARCHAR(255),
   price INTEGER,
   popularity INTEGER,
-  start_time NOW(),
   time_to_complete TIMESTAMP
+  description VARCAR(255)
 );
 
