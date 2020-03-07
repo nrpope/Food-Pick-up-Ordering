94baa0452a80fa1d285eabf9ec41ea4118a3bd34
Bobby Uno <robert.y.uno@gmail.com>
2020-03-07 22:51:37 +0000

    update table within file to correct one

--- db/schema/09_order_items.sql
+++ db/schema/09_order_items.sql
@@ -1,9 +1,9 @@
-DROP TABLE IF EXISTS categories CASCADE;
-CREATE TABLE categories (
+DROP TABLE IF EXISTS order_items CASCADE;
+CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   order_id INTEGER,
   item_id INTEGER,
   quantity INTEGER,
-  PRICE INTEGER
+  price INTEGER
 );
