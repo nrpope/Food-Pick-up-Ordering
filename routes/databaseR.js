f33786ed0bab08f0adcb03e9896bd347c8a51f0c
Bobby Uno <robert.y.uno@gmail.com>
2020-03-08 23:23:14 +0000

    added databaseR.js for my work

---
+++ routes/databaseR.js
@@ -0,0 +1,28 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        //console.log('data.rows', data.rows);
+        const users = data.rows;
+        res.json({ users });
+        //console.log("res.json",  res.json({ users }));
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  //console.log("router", router);
+  return router;
+};
