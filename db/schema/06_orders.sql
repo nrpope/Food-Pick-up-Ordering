17286288b84d1691dd993da8e603b2ef904ce17e
Bobby Uno <robert.y.uno@gmail.com>
2020-03-08 01:17:43 +0000

    added name to orders

--- db/schema/06_orders.sql
+++ db/schema/06_orders.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
   total_price INTEGER,
   start_time TIMESTAMP,
   end_time TIMESTAMP,
---
+++ db/seeds/06_orders.sql
@@ -0,0 +1,1 @@
+INSERT INTO orders (name_id, total_price, start_time, end_time, completed_on) VALUES ()
