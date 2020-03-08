ae8ad1041640c5d407c5e9d6383aaa47afcb3fd9
Bobby Uno <robert.y.uno@gmail.com>
2020-03-08 02:22:00 +0000

    added items seeds

--- db/schema/07_items.sql
+++ db/schema/07_items.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  category_id VARCHAR(255) REFERENCES categories(id),
+  category_id INTEGER REFERENCES categories(id),
   price INTEGER,
   popularity INTEGER,
   time_to_complete INTEGER,
---
+++ db/seeds/07_items.sql
@@ -0,0 +1,5 @@
+INSERT INTO items (name, category_id, price, popularity, time_to_complete, description) VALUES ('Hot dog', 2 ,2, 5, 10, 'A yummy hot dog');
+
+INSERT INTO items (name, category_id, price, popularity, time_to_complete, description) VALUES ('Fries', 5 ,1, 5, 1, 'A bunch of yummy fries');
+
+INSERT INTO items (name, category_id, price, popularity, time_to_complete, description) VALUES ('Pop', 4 ,1, 5, 1, 'A refreshing pop');
