9143b60b72829fce9e82505ccb756fa5e7a80d39
Bobby Uno <robert.y.uno@gmail.com>
2020-03-08 02:37:15 +0000

    added REFERENCES to order_id and item_id

--- db/schema/09_order_items.sql
+++ db/schema/09_order_items.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS order_items CASCADE;
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  order_id INTEGER,
-  item_id INTEGER,
+  order_id INTEGER REFERENCES orders(id),
+  item_id INTEGER REFERENCES items(id),
   quantity INTEGER,
   price INTEGER
 );
