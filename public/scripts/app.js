d3ce7c42e05d33dfad85b737ca825400ade11bc1
Nick Pope <psydpope@gmail.com>
2020-03-09 19:07:01 +0000

    adding function for button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,26 +2,38 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(users => {
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
     }
-  });;
+  });
 });
-
 
 // Experimental code to help with displaying database
 $(() => {
   $.ajax({
     method: "GET",
     url: "/api/categories"
-  }).done((categories) => {
-    for(category of categories) {
-      $("<div>").text(category.name).appendTo($("body"));
+  }).done(categories => {
+    for (category of categories) {
+      $("<div>")
+        .text(category.name)
+        .appendTo($("body"));
     }
-  });;
+  });
 });
-
 
 // End of experimental code
 
+$(".button_cont").submit(function(event) {
+  event.preventDefault();
+  $.ajax({
+    method: "POST",
+    url: "/tweets",
+    data: data
+  }).then(() => {
+    // after POST request make GET request to render the new item
+  });
+});
