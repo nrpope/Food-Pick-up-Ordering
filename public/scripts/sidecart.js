a0606aafa1a9df413a1b8a4cd813f5aada183d43
Nick Pope <psydpope@gmail.com>
2020-03-09 22:52:10 +0000

    adding ajax calls to landing page

--- public/scripts/sidecart.js
+++ public/scripts/sidecart.js
@@ -8,4 +8,30 @@ $(document).ready(function() {
       $(".menu").toggle("slide");
     });
   });
+
+  $(".button_cont").click(function(event) {
+    event.preventDefault();
+    let item_id = 1;
+    // alert("ready to add item" + item_id);
+    $.ajax({
+      method: "POST",
+      url: `/api/add_items/${item_id}`,
+      success: () => {
+        $.ajax({
+          method: "GET",
+          url: "/api/order",
+          success: () => {
+            console.log("success on GET");
+          },
+          error: () => {
+            console.log("error on GET");
+          }
+        });
+        console.log("nothing bad happened yet");
+      },
+      error: () => {
+        console.log("shit shit");
+      }
+    });
+  });
 });
