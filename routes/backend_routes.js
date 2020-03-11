b95c03e25c8cf297b3baeba189b33bb052afe56f
Bobby Uno <robert.y.uno@gmail.com>
2020-03-11 01:58:17 +0000

    minor change

--- routes/backend_routes.js
+++ routes/backend_routes.js
@@ -4,11 +4,11 @@
 
 const express = require('express');
 const router  = express.Router();
-//const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 
 
-//router.use(cookieParser);
+router.use(cookieParser);
 
 
 
