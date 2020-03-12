770d98ba4a2f1d6ec1c600a83dddd7a46db0cc48
Bobby Uno <robert.y.uno@gmail.com>
2020-03-12 22:47:33 +0000

    add to order done

--- routes/backend_routes.js
+++ routes/backend_routes.js
@@ -6,20 +6,36 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = db => {
+
+// example: http://localhost:8080/br
+
   router.get("/", (req, res) => {
     res.json({ test: "TEST" });
   });
-  //add orders to orders table
+
+
+  //add orders to orders table to be submitted
+  // example: http://localhost:8080/br/add/orders
   router.post("/orders", (req, res) => {
-    let order_id = 1;
-    let name = "TEST ORDER3";
-    db.query(
-      `INSERT INTO orders (name, total_price, start_time, end_time, number_of_items, completed) VALUES ($1, 1000, NOW(), '2020-03-10 18:10:10', 2, true)`,
-      [name]
+    let order_id = 10;
+    let name = 'John Smith';
+    db.query(`select count(*) AS numItems, sum(order_items.price*order_items.quantity) AS totalPrice, sum(time_to_complete) AS totalTime FROM order_items JOIN items ON item_id = items.id WHERE order_id = $1 AND quantity > 0`
+    ,
+      [order_id]
     )
       .then(data => {
-        const orders = data.rows;
-        res.json({ orders });
+
+        const {numitems, totalprice, totaltime} = data.rows[0];
+        // console.log('this is data!!!!!!', data.rows[0].numitems);
+        // console.log('this is data!!!!!!', data.rows[0].totalprice);
+        // console.log('this is data!!!!!!', data.rows[0].totaltime);
+        //'2020-03-10 18:10:10'
+        db.query(  `INSERT INTO orders (name, total_price, start_time, end_time, number_of_items, completed) VALUES ($1, $2, NOW(), NOW() + '${totaltime} minute'::interval, $3, false)`, [name, totalprice, numitems])
+
+          .then(data => {
+            const orders = data.rows;
+            res.json({ orders });
+          })
       })
       .catch(err => {
         res.status(500).json({ error: err.message });
@@ -38,13 +54,15 @@ module.exports = db => {
   // });
 
   //display a single order
+  //example: http://localhost:8080/br/orders/1
   router.get("/orders/:order_id", (req, res) => {
-    console.log("req param looks like", req.params.order_id);
+    //console.log("req param looks like", req.params.order_id);
+    let order_id = req.params.order_id;
     db.query(
-      `SELECT *
-              FROM orders
-              WHERE orders.id = $1`,
-      [req.params.order_id]
+      ` SELECT *
+        FROM orders
+        WHERE orders.id = $1`,
+        [order_id]
     )
       .then(data => {
         const orders = data.rows;
