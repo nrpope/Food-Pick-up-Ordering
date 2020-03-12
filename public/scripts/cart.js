6d9825e0f9bb11d5fd984ea37e8d939e39df2097
Nick Pope <psydpope@gmail.com>
2020-03-12 23:43:35 +0000

    added SMS to cart button

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,5 +1,8 @@
+const textCustomer = require("../../routes/SMS");
+
 $(document).ready(function() {
   $("#showCartBtn").click(function() {
+    event.preventDefault();
     console.log("Sup");
     const orderId = $("#order-id").data("orderid");
     $.ajax({
@@ -7,6 +10,7 @@ $(document).ready(function() {
       method: "GET",
       success: resp => {
         console.log("Item count:", resp.data.length);
+        textCustomer();
       },
       error: () => {
         console.log("showOrders failed");
