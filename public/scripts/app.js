35e91cd94a0299c4e89df4266edd812c07bdcb59
Nick Pope <psydpope@gmail.com>
2020-03-12 01:11:43 +0000

    hardcode buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,4 +46,46 @@ $(() => {
   });
 });
 
+$(() => {
+  const orderId = $("#order-id").data("orderid");
+  console.log("i can use this order id now: ", orderId);
+  $(".example_c").on("click", () => {
+    console.log("you want a hot dog!, lets add it to order#: ", orderId);
+    const testData = { order_id: orderId, quantity: 999, price: 0 };
+    $.ajax({
+      url: `/api/order/add/${orderId}`,
+      method: "POST", //POST
+      data: testData // ...order item u want to add to this order
+    }).then(
+      res => {
+        console.log("res from server!", res);
+        res.json({});
+      }
+      //   (res) => { ... }, // on success
+      //   (err) => { ... } // if it errors
+    );
+  });
+});
+
+$(() => {
+  const orderId = $("#order-id").data("orderid");
+  console.log("i can use this order id now: ", orderId);
+  $(".example_c").on("click", () => {
+    console.log("you want a hot dog!, lets add it to order#: ", orderId);
+    const testData = { order_id: orderId, quantity: 999, price: 0 };
+    $.ajax({
+      url: `/api/order/add/${orderId}`,
+      method: "POST", //POST
+      data: testData // ...order item u want to add to this order
+    }).then(
+      res => {
+        console.log("res from server!", res);
+        res.json({});
+      }
+      //   (res) => { ... }, // on success
+      //   (err) => { ... } // if it errors
+    );
+  });
+});
+
 // End of experimental code
