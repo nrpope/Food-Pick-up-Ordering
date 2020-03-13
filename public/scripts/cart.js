6166c05b70caf186c8ff0afa15a95dffa60900ce
Nick Pope <psydpope@gmail.com>
2020-03-13 17:35:52 +0000

    testing sms

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,7 +1,26 @@
-import { textRestaurant } from "./SMS";
+// const textCustomer = require("../../routes/SMS");
 
 $(document).ready(function() {
   $("#showCartBtn").click(function() {
-    textRestaurant();
+    event.preventDefault();
+    console.log("Sup");
+    const orderId = $("#order-id").data("orderid");
+    $.ajax({
+      url: `api/orders`,
+      method: "GET",
+      success: resp => {
+        console.log("Item count:", resp.data.length);
+        // textCustomer();
+      },
+      error: () => {
+        console.log("showOrders failed");
+      }
+    });
+
+    $.ajax({
+      url: `/br/orders`,
+      method: "POST",
+      data: {}
+    });
   });
 });
