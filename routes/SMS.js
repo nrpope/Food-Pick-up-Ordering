453c3f8e17088d5d3ccb23aef67c2eb5a20a0f74
Nick Pope <psydpope@gmail.com>
2020-03-13 16:23:11 +0000

    sms functions and buttons and stuff

--- routes/SMS.js
+++ routes/SMS.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const twilio = require("twilio");
 const client = new twilio(process.env.TWILIO_USER, process.env.TWILIO_AUTHKEY);
 
-function textCustomer(phone) {
+function textCustomer() {
   console.log(phone);
 
   client.messages
     .create({
       body: `Your food will be ready in 20 minutes! Thank you for ordering from LightBistro. Bueno Appetitio.`,
-      to: `${phone}`, // NUMBER WE WANT TO TEXT
+      to: `+17782295929`, // NUMBER WE WANT TO TEXT
       from: "+13342471008" // THIS IS OUR TWILIO SERVER NUMBER
     })
     .then(message => console.log(message.sid));
@@ -28,13 +28,13 @@ function textCustomerPickup(phone) {
     .then(message => console.log(message.sid));
 }
 
-function textRestaurant(orderId) {
+function textRestaurant() {
   console.log();
 
   client.messages
     .create({
-      body: `A new order has been placed
-      ${orderId}
+      body: `A new order has been placed #:
+      123
       please respond with how long in minutes this will take.`,
       to: `6044415470`, // NUMBER WE WANT TO TEXT
       from: "+13342471008" // THIS IS OUR TWILIO SERVER NUMBER
